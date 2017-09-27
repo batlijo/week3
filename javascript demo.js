9bd3caecb37239baeeb970b1f4d20bde506331ac
Batlijo <batlijoselevitz@gmail.com>
2017-09-27 09:33:36 -0700

    added class activities and instructor notes/commentary

---
+++ javascript demo.js
@@ -0,0 +1,6 @@
+javascript demo
+//console to make sure it's working out ok
+console.log(randomStudent);
+//make an alert that says whose name has been called
+alert(randomStudent + "has been called on!");
+	
---
+++ variables.js
@@ -0,0 +1,57 @@
+//basic variables
+var name = "Snow White";
+var dwarfCount =7;
+// Assignment = use for assigning value to variable
+//Strict equality === use for comparisons to see if they're the same
+// GREATER THAN OR LESS THAN > this operates on numbers ie var b = 8 > 3
+// LESS than
+// NOT ! double not would be true
+
+
+//instructor demo
+var fred = 32;
+var i = "e"
+fred = "taco"
+fred = i //you can assign variables to other variables, variables don't need quotation marks.
+//can only have one variable named fred
+
+console.log(fred)
+
+//when you create variables it's always going to be global in browser...
+ //HTML is just a string
+
+ //Conditionals are if/else statements, purpose: ?
+ // a constant is similar to variable except it will not change
+ const name = "William"
+ const age = 28
+ if (name === "William") {
+ 	console.log("Yes, your name is William");
+ }
+
+if (age < 30) {
+	console.log("yes, you're still young");
+} else if (age > 30 && age < 30) ;
+  	console.log("pre-midlife crisis");
+
+const age = 31
+else {
+	console.log("Damn, you're old");
+}
+
+//you can have as many else ifs you want
+// you can have as many ifs you want
+// you don't need to have an else.
+
+//creating an array
+Var a = [  ]
+a=[1]
+a=[1,2,3] //array of numbers
+a=["a","b","c"] //array of strings
+a=[True, False] //booleans
+a[1]= 2 //if you want to get a specific value inside of an array
+a[0]= True
+//DO NOT OVERRIDE YOUR VARIABLES, USE A CONSTANT if you want a safety NET
+var people = ['Daniel', 'Alex' 'Brandon']
+// [  ] means index
+// DO NOT CREATE AN ARRAY WITH DIFFERENT TYPES, BOOLEANS, STRINGS, NUMBERS
+//Length is a property that tells you how many elements are in the arrays
